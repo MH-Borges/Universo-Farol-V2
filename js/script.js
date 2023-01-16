028879977f1316dcf807d9de18ca576d20a7dab6
Matheus Henrique Borges <matheush.borges@hotmail.com>
2023-01-16 15:34:07 -0300

    Base commit (background + Header)

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+        $(".toTop").fadeIn();
+    } else {
+        $(".toTop").fadeOut();
+    }
+});
+
+$(document).on("click", ".toTop", function () {
+    $("html, body").animate({ scrollTop: 0 }, 400);
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var height = $(window).height();
+    if (scroll < height) {
+        $(".zoom img").css({
+            transform: 'translate3d(-'+ (50+(scroll / 100)*-11) +'%, '+ ((scroll / 100)*5.5) + '%, 0) scale(' + (100 + scroll / 2) / 100 + ')',
+        });
+    }
+    if(scroll > height){
+        console.log(scroll);
+        console.log('entrei');
+        console.log(height);
+    }
+});
