c3b6fcaec581a7ad62613a1a3efb75e56245fbaa
Matheus Henrique Borges <matheush.borges@hotmail.com>
2023-01-20 21:38:03 -0300

    Farol Animated

--- js/script.js
+++ js/script.js
@@ -1,28 +1,88 @@
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     if (scroll >= 100) {
-        $(".toTop").fadeIn();
+      $(".toTop").fadeIn();
     } else {
-        $(".toTop").fadeOut();
+      $(".toTop").fadeOut();
     }
-});
-
-$(document).on("click", ".toTop", function () {
+  });
+  
+  $(document).on("click", ".toTop", function () {
     $("html, body").animate({ scrollTop: 0 }, 400);
-});
-
-
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    var height = $(window).height();
-    if (scroll <= height) {
-        $(".zoom img").css({
-            transform: 'translate3d(-'+ (50+(scroll / 100)*-11) +'%, '+ ((scroll / 100)*17) + '%, 0) scale(' + (100 + scroll / 2.5) / 100 + ')',
-        });
-    }
-    if(scroll > height){
-        console.log(scroll);
-        console.log('entrei');
-        console.log(height);
-    }
-});
+  });
+  
+  
+  var images = gsap.utils.toArray(".images");
+  images.forEach((image, i) => {
+    gsap.fromTo(
+      '.images',
+      {
+        scale: 1,
+      },
+      {
+        scale: 2.3,
+        ease: "none",
+        xPercent: 33,
+        yPercent: 18,
+        scrollTrigger: {
+          pin: jQuery(image).parent(),
+          trigger: jQuery(image).parent(),
+          start: "top top",
+          end: "bottom top",
+          scrub: 0.5,
+        }
+      }
+    );
+    gsap.fromTo(
+      '.tempes',
+      {
+        opacity: 1
+      },
+      {
+        opacity: 0,
+        ease: "none",
+        scrollTrigger: {
+          start: "top top",
+          end: "20% top",
+          scrub: true,
+        }
+      }
+    );
+  
+  });
+  
+  var Bannerlines = gsap.utils.toArray(".Banner");
+  Bannerlines.forEach((elem, i) => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: elem,
+        start: "-200 0%",
+        end: "+=300 20%",
+        scrub: true,
+      }
+    });
+  
+    tl
+      .to(elem, { opacity: 1, duration: 0.5, y: 0 })
+      .to(elem, { opacity: 0, duration: 0.5, y: -50 }, 0.8);
+      ;
+  
+  });
+  
+  var headlines = gsap.utils.toArray(".Text");
+  headlines.forEach((elemt, i) => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: elemt,
+        start: "top top",
+        end: "+=400 top",
+        scrub: true,
+      }
+    });
+  
+    tl
+      .to(elemt, { opacity: 0, duration: 0.5 })
+      .to(elemt, { opacity: 1, duration: 0.5 });
+      ;
+  
+  });
