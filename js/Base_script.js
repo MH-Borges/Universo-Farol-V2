a0ac6e715ff8523259fa61eec1c5dc9c1835d655
Matheus Henrique <matheush.borges@hotmail.com>
2024-10-21 22:48:25 -0300

    portfolio + ajustes gerais

--- js/Base_script.js
+++ js/Base_script.js
@@ -29,45 +29,6 @@ function sideMenu(){
     }
 }
 
-//Smooth Scroll
-document.querySelectorAll('.links').forEach(item => { item.addEventListener('click', scrollToIdOnClick); });
-function scrollToIdOnClick(event) {
-    const targetElement = document.querySelector(event.currentTarget.getAttribute('href'));
-    if (targetElement) {
-        if(event.currentTarget.getAttribute('href') == '#home'){
-            var targetOffset = targetElement.offsetTop;
-        }else{
-            var targetOffset = targetElement.offsetTop - 150;
-        }
-        smoothScrollTo(0, targetOffset);
-    }
-}
-function smoothScrollTo(endX, endY, duration = 200) {
-    const startX = window.scrollX || window.pageXOffset;
-    const startY = window.scrollY || window.pageYOffset;
-    const distanceX = endX - startX;
-    const distanceY = endY - startY;
-    const startTime = performance.now();
-    const easeInOutQuart = (time, from, distance, duration) => {
-        if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-        return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-    };
-    function scroll() {
-        const currentTime = performance.now();
-        const elapsedTime = currentTime - startTime;
-        if (elapsedTime >= duration) {
-            window.scroll(endX, endY);
-        } else {
-            const newX = easeInOutQuart(elapsedTime, startX, distanceX, duration);
-            const newY = easeInOutQuart(elapsedTime, startY, distanceY, duration);
-            window.scroll(newX, newY);
-            requestAnimationFrame(scroll);
-        }
-    }
-    scroll();
-}
-
-
 //Scroll toTop
 window.addEventListener('scroll', function() {
     if(window.pageYOffset > 350){ 
@@ -77,6 +38,7 @@ window.addEventListener('scroll', function() {
         document.querySelector(".toTop").classList.add('hide'); 
     }
 });
+
 function ancora(){
     document.querySelector(".Anc").classList.add('hide');
     document.querySelector(".Anc_Q").classList.remove('hide');
